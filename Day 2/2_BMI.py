2b4060ecc339189b0c9c4a53103dd5b1f590d9a2
itzzmevishal <vishal9304960503@gmail.com>
2023-01-19 17:04:35 +0530

    Day 2 Code

--- Day 2/2_BMI.py
+++ Day 2/2_BMI.py
@@ -1,10 +1,18 @@
+# Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+# The BMI is a measure of someone's weight taking into account their height. e.g.
+# If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
+
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+
 # Calculate the BMI(Body Mass Index) of the Body
+
+# ANSWER👇👇👇👇👇👇👇
 
 # 🚨 Don't change the code below 👇
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-BMI=(int(weight) / float(height)**2)
+# Write your code below this line 👇
+BMI = (int(weight) / float(height)**2)
 print(int(BMI))
